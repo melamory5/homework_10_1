717c43a308e9d8b07591b000b57a3b4f4936b75c
Milena_Vishnevskaia <vishnevskaia.milena@mail.ru>
2025-11-30 22:01:26 +0300

    after_linters

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dicts: list, state: str="EXECUTED") -> list:
+def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     """Функция, принимающая список словарей, возвращаеет новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению"""
     new_list_of_dicts = []
@@ -9,7 +9,7 @@ def filter_by_state(list_of_dicts: list, state: str="EXECUTED") -> list:
     return new_list_of_dicts
 
 
-def sort_by_date(list_of_dicts: list, reverse: bool=True) -> list:
+def sort_by_date(list_of_dicts: list, reverse: bool = True) -> list:
     """Функция, принимающая список словарей, возвращает новый список словарей, отсортированный по дате"""
     sorted_list_of_dicts = sorted(list_of_dicts, key=lambda x: x.get('date', ''), reverse=reverse)
 
